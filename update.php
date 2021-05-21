1a6c1a78cab526681a2cb23d5c2f5f27e9567785
Aurelle Wafo <59320937+AuriStark@users.noreply.github.com>
2021-05-21 09:26:21 +0200

    Location in savedate and updatedate updated

--- update.php
+++ update.php
@@ -6,20 +6,20 @@
 </div>
     <h2 class="mb-2"> Edit Record </h2>
     
-    <form class="post-form" action="updatedata.php" method="post" >
+    <form class="post-form" action="update.php" method="post" >
        
         <div class="form-group row">
             <label for="idFormControl">Id</label>
-            <input type="id" class="form-control" id="idFormControl" placeholder="id" name="sid" required/>
+            <input type="id" class="form-control" id="idFormControl" placeholder="id" name="sid"/>
         </div>
         <div class="form-group">
-            <input type="button" value="Show" name="showbtn"  class="btn">
+            <input type="submit" value="Show" name="showbtn"  class="btn">
         </div> 
     </form>
 
     <?php
         if(isset($_POST['showbtn'])){
-            echo "Daine";
+            #echo "Daine";
             include 'config.php';
 
             $stu_id=$_POST['sid'];
@@ -35,35 +35,20 @@
         <div class="form-group row">
             <label for="nameFormControl">Name</label>
             <input type="hidden" name="sid" value="<?php echo $row['sid']; ?>" />
-            <input type="name" class="form-control" id="nameFormControl" placeholder="name" name="sname" value="<?php echo $row['sname'];?>" required/>
+            <input type="text" class="form-control" id="nameFormControl" placeholder="name" name="sname" value="<?php echo $row['sname'];?>"/>
         </div>
         <div class="form-group row">
             <label for="adressFormControl">Adress</label>
-            <input type="adress" class="form-control" id="adressFormControl" placeholder="adress" name="sadress" value="<?php echo $row['sadress'];?>" required/>
+            <input type="text" class="form-control" id="adressFormControl" placeholder="adress" name="sadress" value="<?php echo $row['saddress'];?>"/>
         </div>
         <div class="form-group row">
             <label for="classFormControl">Class</label>
-            <select class="form-control" id="classFormControl" value="<?php echo $row['sclass'];?>" required>
-            <option>Select Class</option>
-
-            <?php 
-                include "config.php";
-                $sql ="SELECT * FROM studentclass" ;
-                $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
-
-                while($row = mysqli_fetch_assoc($result)){
-            ?>
-            <option> 
-                <?php echo $row['cname'];?>
-            </option>
             
-            <?php } ?>
 
-            </select>
         </div>
         <div class="form-group">
             <label for="phoneFormControl">Phone</label>
-            <input type="phone" class="form-control" id="phoneFormControl" placeholder="phone" name="sphone" value="<?php echo $row['sphone'];?>" required/>
+            <input type="text" class="form-control" id="phoneFormControl" placeholder="phone" name="sphone" value="<?php echo $row['sphone'];?>"/>
         </div>  
         <div class="form-group">
             <input type="submit" value="Update" class="btn" name="updatebtn">
@@ -72,8 +57,6 @@
 
     <?php
             }
-        }else{
-            echo"Id not found";
         }
     }
     ?>
--- updatedata.php
+++ updatedata.php
@@ -7,10 +7,10 @@
 
     $conn = mysqli_connect("localhost", "root","","crud") or die("Connection Failed");
 
-    $sql = "UPDATE student SET sname = '{$stu_name}', sadress ='{$stu_adress}', sclass = '{$stu_class}', sphone = '{$stu_phone}' WHERE sid ={$stu_id}";
+    $sql = "UPDATE student SET sname = '{$stu_name}', saddress ='{$stu_adress}', sclass = '{$stu_class}', sphone = '{$stu_phone}' WHERE sid ={$stu_id}";
     $resullt = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
 
-    header("Location:http//localhost/crud-php/index.php");
+    header("Location: http://localhost/My-Projects/crud-php/index.php");
 
     mysqli_close($conn);
 ?>
